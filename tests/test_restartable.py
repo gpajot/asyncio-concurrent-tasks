8b8b0fc1682cbbdcf653c922a0834b43dd30eea6
Gabriel Pajot <github@gab.les-cactus.co>
2023-01-04 13:19:35 +0100

    fix: support restartable funcs returning futures

--- tests/test_restartable.py
+++ tests/test_restartable.py
@@ -17,25 +17,21 @@ class TestRestartableTask:
     def task(self, func):
         return RestartableTask(func, timeout=0.01)
 
-    @pytest.mark.asyncio()
     async def test_not_started(self, task):
         with pytest.raises(RuntimeError, match="has not been started"):
             await task
 
-    @pytest.mark.asyncio()
     async def test_already_started(self, task):
         task.start()
         with pytest.raises(RuntimeError, match="is already running"):
             task.start()
         task.cancel()
 
-    @pytest.mark.asyncio()
     async def test_timeout(self, task):
         task.start()
         with pytest.raises(asyncio.TimeoutError):
             await task
 
-    @pytest.mark.asyncio()
     async def test_restart(self, task, func):
         task.start()
         await asyncio.sleep(0)  # context switch to let the task start
@@ -46,34 +42,53 @@ class TestRestartableTask:
         await task
         assert func.call_count == 2
 
-    @pytest.mark.asyncio()
     async def test_success(self, task):
         task.start()
         task.set_result(1)
         assert await task == 1
 
-    @pytest.mark.asyncio()
     async def test_failure(self, task):
         task.start()
         task.set_exception(ValueError("error"))
         with pytest.raises(ValueError, match="error"):
             await task
 
+    async def test_partial_func(self, mocker):
+        func = mocker.Mock()
+        task = RestartableTask(partial(func, 1), timeout=0.01)
+        task.start()
+        await asyncio.sleep(0)  # context switch to let the task start
+        task.cancel()
+        await asyncio.sleep(0)  # context switch to let the task cancel
+        func.assert_called_with(1)
+
+    async def test_partial_async_func(self):
+        called = False
+
+        async def func(arg: int):
+            assert arg == 1
+            nonlocal called
+            called = True
+
+        task = RestartableTask(partial(func, 1), timeout=0.01)
+        task.start()
+        await asyncio.sleep(0)  # context switch to let the task start
+        task.cancel()
+        await asyncio.sleep(0)  # context switch to let the task cancel
+        assert called is True
+
     @pytest.mark.skipif(
         sys.version_info < (3, 8), reason="requires python3.8 or higher for AsyncMock"
     )
-    @pytest.mark.asyncio()
-    async def test_partial_async_func(self, mocker):
+    async def test_partial_async_func_mock(self, mocker):
         func = mocker.AsyncMock(spec=types.FunctionType)
         task = RestartableTask(partial(func, 1), timeout=0.01)
         task.start()
         await asyncio.sleep(0)  # context switch to let the task start
         task.cancel()
         await asyncio.sleep(0)  # context switch to let the task cancel
-        await asyncio.sleep(0)  # context switch to let the task cancel
         func.assert_awaited_once_with(1)
 
-    @pytest.mark.asyncio()
     async def test_external_cancel(self, task):
         task.start()
         with pytest.raises(asyncio.TimeoutError):
