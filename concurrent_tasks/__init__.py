9074e23e3c9744d3b4e7602d99cd64e1547822ba
Gabriel Pajot <github@gab.les-cactus.co>
2023-02-12 14:26:28 +0100

    feat(threaded pools): add context manager wrappping util

--- concurrent_tasks/__init__.py
+++ concurrent_tasks/__init__.py
@@ -1,7 +1,12 @@
+import logging
+
 from concurrent_tasks.background import BackgroundTask
 from concurrent_tasks.restartable import RestartableTask
 from concurrent_tasks.thread_safe_pool import ThreadSafeTaskPool
 from concurrent_tasks.threaded_pool import (
     AsyncThreadedTaskPool,
     BlockingThreadedTaskPool,
+    ThreadedPoolContextManagerWrapper,
 )
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
--- concurrent_tasks/threaded_pool/__init__.py
+++ concurrent_tasks/threaded_pool/__init__.py
@@ -1,2 +1,3 @@
 from concurrent_tasks.threaded_pool.aio import AsyncThreadedTaskPool
+from concurrent_tasks.threaded_pool.base import ThreadedPoolContextManagerWrapper
 from concurrent_tasks.threaded_pool.blocking import BlockingThreadedTaskPool
