80161ed777afd5ef9acebddc051849866b44cdd5
Gabriel Pajot <github@gab.les-cactus.co>
2024-02-01 17:43:11 +0100

    feat: add PeriodicTask

--- concurrent_tasks/__init__.py
+++ concurrent_tasks/__init__.py
@@ -1,6 +1,7 @@
 import logging
 
 from concurrent_tasks.background import BackgroundTask
+from concurrent_tasks.periodic import PeriodicTask
 from concurrent_tasks.restartable import RestartableTask
 from concurrent_tasks.thread_safe_pool import ThreadSafeTaskPool
 from concurrent_tasks.threaded_pool import (
---
+++ concurrent_tasks/periodic.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import asyncio
+from time import monotonic
+from typing import Awaitable, Callable, Generic, TypeVar
+
+from typing_extensions import ParamSpec
+
+from concurrent_tasks.background import BackgroundTask
+
+T = TypeVar("T")
+P = ParamSpec("P")
+
+
+class PeriodicTask(BackgroundTask, Generic[P]):
+    """Run an async task periodically in the background.
+
+    Note: there is no guarantee that the time between calls is strictly the interval
+    if the function takes more time than the interval to execute.
+    """
+
+    def __init__(
+        self,
+        interval: float,
+        func: Callable[P, Awaitable],
+        *args: P.args,
+        **kwargs: P.kwargs,
+    ):
+        super().__init__(self._run, interval, *args, **kwargs)
+        self._initial_func = func
+
+    async def _run(self, interval: float, *args: P.args, **kwargs: P.kwargs):
+        while True:
+            start = monotonic()
+            await self._initial_func(*args, **kwargs)
+            if remaining := interval - (monotonic() - start):
+                await asyncio.sleep(remaining)
